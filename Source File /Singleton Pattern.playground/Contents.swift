db98a2ae0b5fcd2a60433a20847dd261164ce864
Ahmed Amin <Ahmedaminhassanismail@gmail.com>
2021-06-21 09:34:06 +0200

    Build Singleton Pattern and modify Readme file

---
+++ Source File /Singleton Pattern.playground/Contents.swift
@@ -0,0 +1,23 @@
+
+import Foundation
+import UIKit
+
+// MARK: - Singleton Pattern
+
+ /*
+ consider that you create setting screen for your app
+ you have a listed of data in tableview and you have
+ option in setting that display this data either sequential data or random data
+ */
+
+class AppSetting {
+    
+    static let shared = AppSetting()
+    
+    private init() { }
+    
+    func getLanguage() {
+        
+    }
+}
+
